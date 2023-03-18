b3d1704f0b2b5ed406682e478ec48d17c3bbea0e
hyz0247 <2472064879@qq.com>
2023-03-18 22:51:12 +0800

    更新

---
+++ src/components/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button></el-button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "register"
+}
+</script>
+
+<style scoped>
+
+</style>
