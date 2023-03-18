aa779ce19888364f798a5b13d705524ffa999bca
hyz0247 <2472064879@qq.com>
2023-03-18 19:14:42 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueRouter from "vue-router";
+import router from "@/router";
+import store from "@/store";
 
+
+
+
+Vue.prototype.$axios =axios;
+axios.defaults.withCredentials = true
+Vue.prototype.$httpUrl='http://localhost:8082'
+
+
+Vue.use(VueRouter);
+Vue.use(ElementUI,{size:'small'});
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
